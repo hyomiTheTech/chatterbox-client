727322068dbf93dc148b49b7d73a36040ae5f71f
hyomithetech <hyomithetech@gmail.com>
2019-11-01 21:56:16 -0700

    Add initialize method

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -1,12 +1,19 @@
 var RoomsView = {
 
-  $button: $('#rooms button'),
+  $button: $('#rooms button').on('click', Rooms.add),
   $select: $('#rooms select'),
 
   initialize: function() {
+    Parse.readAll(data => {
+      data.results.forEach(msg => {
+        this.renderRoom(msg.roomname);
+      });
+    });
   },
 
-  render: function() {
+  renderRoom: function(roomName) {
+    var newOption = `<option value='${roomName}'>${roomName}</option>`;
+    this.$select.append(newOption);
   }
 
 };
