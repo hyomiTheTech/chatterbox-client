e4e7454c7ff648e52f8506c95c473c5de3eb3eba
hyomithetech <hyomithetech@gmail.com>
2019-11-02 14:41:58 -0700

    Implement create method on messages.js

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -1,4 +1,16 @@
 var Messages = {
 
+  //create method
+  create: function (text) {
+    return {
+      username: App.username,
+      text: text,
+      roomname: RoomsView.currentRoom()
+    };
+  }
+  // takes text as param
+  //  grab room = RoomsView.currentRoom;
+  //  username = App.username;
+  // return messageInstance
 
 };
