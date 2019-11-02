00621e44c4c7a2962c7f0d8be28e26387b1b998c
hyomithetech <hyomithetech@gmail.com>
2019-11-02 16:55:36 -0700

    Implement toggleStatus method in friends.js

--- client/scripts/friends.js
+++ client/scripts/friends.js
@@ -1,4 +1,11 @@
 var Friends = {
+  toggleStatus: function(friend) {
+    var array = Array.prototype.slice.apply(document.querySelector('#chats').children);
+    array.forEach(c => {
+      if (c.children[0].innerText === friend) {
+        c.classList.add('friend');
+      }
+    });
 
-
+  }
 };
