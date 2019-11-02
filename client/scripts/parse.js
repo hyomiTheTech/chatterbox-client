75ef86d186499a9b62234f46cdc145f240cf87ec
hyomithetech <hyomithetech@gmail.com>
2019-11-01 21:55:13 -0700

    Remove success callback method

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -1,13 +1,7 @@
 var Parse = {
 
   server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
-  // add successCB method
-  success: function(data) {
-    // log success message and data
-    console.log(`Data: ${data}`);
-    console.log('Success');
-  },
-  // review usage of success callback
+
   create: function(message, successCB, errorCB = null) {
     // todo: save a message to the server
     // create ajax request
@@ -17,7 +11,7 @@ var Parse = {
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
-      success: this.success,
+      success: successCB,
       error: errorCB || function(error) {
         console.error('chatterbox: Failed to fetch messages', error);
       }
