a52e22da5a5a2b03b1c278e5b401bab7237af3ef
hyomithetech <hyomithetech@gmail.com>
2019-11-01 18:59:00 -0700

    Write render message method on messagesView.js

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -5,7 +5,17 @@ var MessagesView = {
   initialize: function() {
   },
 
-  render: function() {
+  renderMessage: function(message) {
+    // build underscore function to create templates
+    var compiled = _.template(`
+      <div class='message'>
+        <div><%- username %></div>
+        <div><%- text %></div>
+      </div>
+    `);
+    // use the underscore function to create templates with the message
+    // append the templates to the html file
+    this.$chats.append(compiled(message));
   }
 
 };
