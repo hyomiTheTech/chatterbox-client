33568e13f6d7ec9e904938b934896c6077da5de4
hyomithetech <hyomithetech@gmail.com>
2019-11-01 19:43:13 -0700

    Remove functionality from render method that is not it's concern

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -6,16 +6,8 @@ var MessagesView = {
   },
 
   renderMessage: function(message) {
-    // build underscore function to create templates
-    var compiled = _.template(`
-      <div class='message'>
-        <div><%- username %></div>
-        <div><%- text %></div>
-      </div>
-    `);
-    // use the underscore function to create templates with the message
-    // append the templates to the html file
-    this.$chats.append(compiled(message));
+    // append to DOM
+    this.$chats.append(MessageView.render(message));
   }
 
 };
